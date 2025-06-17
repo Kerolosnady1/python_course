ab6f82cdd1c2bfd9e848251dd48f6b7338249cb8
kerolosnady2 <kerolosnady777@gmail.com>
2025-06-17 14:21:42 +0300

    Variables && DataTypes

---
+++ 2_varibales.py
@@ -0,0 +1,25 @@
+# Variables & Data Types
+
+x = 5.5 # float
+print(type(x)) # class 'float'
+
+studentName = 'Kerolos' # identifier # string(text) => str # camelCase
+
+student_age = 20 # underscore_case
+
+print(studentName) # Kerolos # 0xf500 => y => points to => Kerolos => Memory
+
+integers = 60 # int
+print(integers)
+
+boolean = True # Bool => boolean
+print(boolean) # True 
+
+boolean2 = False # Bool => Boolean
+print(boolean2) # False
+
+# 1boolean3 = True # Error => Number at the first of the variable name
+
+# @floating_number = 3.33 # Error => Special Character @, #, ...
+
+
