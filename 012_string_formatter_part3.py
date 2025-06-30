110fefc3a53298fc533f8f13509f7a2485b214db
kerolosnady2 <kerolosnady777@gmail.com>
2025-06-30 10:06:19 +0300

    String Formatter Part Three

---
+++ 012_string_formatter_part3.py
@@ -0,0 +1,16 @@
+# String Formatter Part 3
+
+# Version 3.6 and after:
+
+name = "Kerolos"
+age = 20
+
+print(f"My name is {name}, and my age is {age}") # My name is Kerolos, and my age is 20
+
+print(f"Number: {age:.2f}") # Number: 20.00
+
+msg = "Hello my name is Kerolos"
+
+print(f"Long Message: {msg}") # Long Message: Hello my name is Kerolos
+
+print(f"Short Message: {msg:.5s}") # Short Message: Hello
