9855daff365eaa5eb6c6fcb8e99febe116bba43d
kerolosnady2 <kerolosnady777@gmail.com>
2025-07-16 15:36:05 +0300

    Identity Operators

---
+++ 017_identity_operators.py
@@ -0,0 +1,51 @@
+# Identity Operators:
+    
+    # is
+    # is not
+
+# is : True if both are same
+
+# print(10 is 10) # syntaxwarning: 'is' with 'int' literal. Did you mean '=='? (True)
+
+# print("Hello" is "Hello") # syntaxwarning: 'is' with 'str' literal. Did you mean '=='? (True)
+
+numberOne = 10
+numberTwo = 10
+print(numberOne is numberTwo) # True
+
+print("*" * 30)
+
+textOne = "Hello"
+textTwo = "hello"
+print(textOne is textTwo) # False
+
+print("*" * 30)
+
+
+# is not : True if Both are not the same
+
+numberThree = 30
+numberFour = 50
+print(numberThree is not numberFour) # True
+
+print("*" * 30)
+
+textThree = "Kero"
+textFour = "Kero"
+print(textThree is not textFour) # False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
