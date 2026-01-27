6f8dd75882b36b26a3db115f352cd1aab57918d3
Kerolosnady1 <kerolosnady444@gmail.com>
2026-01-27 22:20:35 +0200

    Introduction to web

---
+++ 000-Intro_to_web.py
@@ -0,0 +1,17 @@
+# Kerolos Nady
+# 21
+# Courses => Software Engineering ALXafrica / Back-end web app development using django / Software testing sprints.ai / Linux unhatched & Essentials
+# Web Application using Django
+# User => Client Side
+# Database & back-end => Server Side
+# Protocol => HTTP/HTTPS -> Hyper Text Transformation Protocol Secure
+# Request => طلب 
+# Response => اجابه
+# Web Page => Only one page
+# Website => many pages in one site under one domain
+# Web App => Application + Website (Applications linked to each other)
+# Web 1.0 (1990 - 2004) => Read Only web (Static pages)
+# Web 2.0 (2004 - Now) => Read & Write (Static & Dynamic Pages) -> (EX: Socail Media (Facebook))
+# Web 3.0 (New Generation of 2.0) => Read & Write (Static & Dynamic) -> (EX: AI, Blockchain)
+# Django (Python) => (AI / Back-end Web / Data / Games / Machine Learning)
+# Django(Python) VS ASP.NET core(C#) VS Laravel(PHP)
